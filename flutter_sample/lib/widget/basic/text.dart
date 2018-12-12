3c5112e2cc99cb846044c0ca3fc24a6e36bf38d8
yzq <1149635502@qq.com>
2018-12-12 20:39:23 +0800

    完善Text Widget示例

--- flutter_sample/lib/widget/basic/text.dart
+++ flutter_sample/lib/widget/basic/text.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 class MyText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return NormalText();
+    return Column(
+      children: <Widget>[
+        NormalText(),
+        MyTextSpan(),
+        MyDefaultStyleText(),
+      ],
+    );
   }
 }
 
@@ -17,12 +23,12 @@ class NormalText extends StatelessWidget {
       "Flutter is Google’s mobile UI framework for crafting high-quality native interfaces on iOS and Android in record time. Flutter works with existing code, is used by developers and organizations around the world, and is free and open source.",
       style: TextStyle(
           color: Colors.red,
-          fontSize: 18,
+          fontSize: 20,
           letterSpacing: 1,
           wordSpacing: 2,
           height: 1.2,
           fontWeight: FontWeight.w600),
-      textAlign: TextAlign.justify,
+      textAlign: TextAlign.left,
       maxLines: 3,
       overflow: TextOverflow.ellipsis,
     );
@@ -32,16 +38,26 @@ class NormalText extends StatelessWidget {
 /*TextSpan
 * 文本片段，可以对文本片段单独设置样式
 * */
-class MySpanText extends StatelessWidget {
+class MyTextSpan extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text.rich(
       TextSpan(
         children: [
-          TextSpan(text: "网址:"),
+          TextSpan(
+              text: "网址:",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              )),
           TextSpan(
             text: "http//:baidu.com/",
-            style: TextStyle(color: Colors.blue),
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 24,
+              fontStyle: FontStyle.italic,
+            ),
           ),
         ],
       ),
@@ -54,17 +70,34 @@ class MyDefaultStyleText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTextStyle(
-      textAlign: TextAlign.left,
-      style: TextStyle(
-        fontSize: 18,
-        color: Colors.grey,
-        letterSpacing: 1,
-        fontWeight: FontWeight.w500,
-      ),
-      child: Column(
-        children: <Widget>[NormalText(), MySpanText()],
-      ),
-    );
+        /*这里我们设置了一些默认的样式*/
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.grey,
+          letterSpacing: 1,
+          wordSpacing: 3,
+          fontWeight: FontWeight.bold,
+        ),
+        /*子节点就会默认继承父节点的样式，如果子节点重新设置了父节点中已设置的样式，那么以子节点设置的样式为准*/
+        child: Column(
+          children: <Widget>[
+            Text(
+              "text 1",
+              style: TextStyle(
+                /*这里我们重新指定一下颜色，那么最终的颜色就以子节点的设置为准*/
+                color: Colors.deepOrange,
+              ),
+            ),
+            Text(
+              "text 2",
+              style: TextStyle(
+                inherit: false, //inherit设为false表示不继承父节点默认样式，默认值为true
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ));
     ;
   }
 }

ccf922a073b5f383840f39dcee7ca26c71236522
yzq <1149635502@qq.com>
2019-03-04 16:28:39 +0800

    新增底部导航栏示例

--- flutter_sample/lib/main.dart
+++ flutter_sample/lib/main.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sample/widget/multilayout/column.dart';
-import 'package:flutter_sample/widget/multilayout/customMultiChildLayout.dart';
-import 'package:flutter_sample/widget/multilayout/expanded.dart';
-import 'package:flutter_sample/widget/multilayout/flow.dart';
-import 'package:flutter_sample/widget/multilayout/gridview.dart';
-import 'package:flutter_sample/widget/multilayout/index_stack.dart';
-import 'package:flutter_sample/widget/multilayout/listbody.dart';
-import 'package:flutter_sample/widget/multilayout/listview.dart';
-import 'package:flutter_sample/widget/multilayout/row.dart';
-import 'package:flutter_sample/widget/multilayout/stack.dart';
-import 'package:flutter_sample/widget/multilayout/table.dart';
-import 'package:flutter_sample/widget/multilayout/wrap.dart';
+import 'package:flutter_sample/widget/bottom_nav/index_page.dart';
 
 /*入口函数*/
 void main() => runApp(MyApp());
@@ -21,14 +10,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter入门示例程序',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter"),
-        ),
-        body: ListViewBuilder(),
-      ),
+      home: IndexPage(),
     );
   }
 }
